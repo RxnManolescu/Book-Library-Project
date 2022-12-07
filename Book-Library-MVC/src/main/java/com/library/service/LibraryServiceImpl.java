9034ab66d76c75b664aa4bbbf4a0e1fb7c8e2615
ash-g773 <aishwaryageorge@ymail.com>
2022-12-07 15:05:41 +0000

    making service layers

--- Book-Library-MVC/src/main/java/com/library/service/LibraryServiceImpl.java
+++ Book-Library-MVC/src/main/java/com/library/service/LibraryServiceImpl.java
@@ -1,8 +1,11 @@
 package com.library.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.library.entity.Library;
 import com.library.persistence.LibraryDao;
 
 @Service
@@ -10,4 +13,22 @@ public class LibraryServiceImpl implements LibraryService {
 
 	@Autowired
 	private LibraryDao libraryDao;
+
+	@Override
+	public boolean borrowBook(int employeeId, int bookId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean returnBook(int employeeId, int bookId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Library> getBooksByEmployeeId(int employeeId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- Employee-Service/src/main/java/com/employee/service/EmployeeServiceImpl.java
+++ Employee-Service/src/main/java/com/employee/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.employee.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.employee.entity.Employee;
 import com.employee.persistence.EmployeeDao;
 
 @Service
@@ -10,5 +11,17 @@ public class EmployeeServiceImpl implements EmployeeService {
 	
 	@Autowired
 	private EmployeeDao employeeDao;
+
+	@Override
+	public Employee checkLoginIdAndPassword(int id, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean changeBookQuantity(int id, int changeQuantity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
