21fc503685f9986b24745a5dc549f70c0caa7b40
Roxana Manolescu <rxn_manolescu@yahoo.com>
2022-12-13 09:39:29 +0000

    update

--- Book-Library-MVC/src/main/java/com/library/resources/LibraryResources.java
+++ Book-Library-MVC/src/main/java/com/library/resources/LibraryResources.java
@@ -1,8 +1,14 @@
 package com.library.resources;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.library.entity.Employee;
 import com.library.service.LibraryService;
 
 @RestController
@@ -10,4 +16,26 @@ public class LibraryResources {
 
 	@Autowired
 	private LibraryService libraryService;
+	
+	@RequestMapping("/")
+	public ModelAndView getLoginPageController() {
+		return new ModelAndView("index");
+	}
+
+	@RequestMapping("/login")
+	public ModelAndView loginController(@RequestParam("employeeId") int id,@RequestParam("pwd") String password,HttpSession session) {
+		ModelAndView modelAndView=new ModelAndView();
+		
+		
+//		if(employee!=null) {
+//			session.setAttribute("employee", employee);
+//			modelAndView.setViewName("	");
+//		}
+//		else {
+//			modelAndView.addObject("message", "Login Failed, Please try again");
+//			modelAndView.setViewName("index");
+//		}
+
+		return modelAndView;
+	}
 }
--- Book-Library-MVC/src/test/java/com/library/main/BookLibraryMvcApplicationTests.java
+++ Book-Library-MVC/src/test/java/com/library/main/BookLibraryMvcApplicationTests.java
@@ -1,4 +1,4 @@
-package com.employee.main;
+package com.library.main;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
