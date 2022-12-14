6bb7be593ab31bd9be8e176981c41de88eed3bfd
nataliaarcillarequena <113618302+nataliaarcillarequena@users.noreply.github.com>
2022-12-14 00:10:04 +0000

    14 Dec

---
+++ Book-Library-MVC-f/src/main/java/com/library/service/LibraryService.java
@@ -0,0 +1,39 @@
+package com.library.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.library.entity.Book;
+import com.library.entity.Employee;
+import com.library.entity.Library;
+
+public interface LibraryService {
+	
+//	public Library borrowBook(Employee employee, Book book);
+//	
+//	public Library returnBook(Library library);
+//	
+//	public List<Library> getLibrariesByEmployeeId(int employeeId);
+	
+	//NAT HERE ---------------------------------------------------
+	public List<Book> getBookList();
+	
+	public Library borrowBook2(int bookId, int copies, Employee employee);
+	
+	public List<Library> getBorrowedBooks();
+	
+	public Library returnBook2(String transactionId, int copies);
+	
+	public Library getRecord(String transactionId);
+
+	Employee loginCheck(int id, String password);
+
+	List<Library> getLibraryByEmployeeId(int employeeId);
+	
+	//they want us to take the type of book to be returned and issue date of that book 
+	//how many copies of that book to return ? 
+	
+	List<Library> getBooksByTypeAndDate(String type, LocalDate date, int empId);
+	
+	
+}
